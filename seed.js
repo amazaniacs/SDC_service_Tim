61a143b9735d33696d688a9043b42fabf9edc262
Tingbin Huang <tingbinhuang@Tingbins-MacBook-Pro.local>
2019-03-01 21:31:16 -0800

    Update seeding file

--- seed.js
+++ seed.js
@@ -1,25 +1,7 @@
 var fs = require('fs');
-var wstream = fs.createWriteStream('myData.csv');
+var wstream = fs.createWriteStream('data.csv');
 
-// feedToArr function 
-let feedToArr = (data, number = 10000000) => {
-
-  for (let j = 0; j < number; j++) {
-
-    let obj = {
-      id: j + 1,
-      name: `Amazon_Product${j + 1}`,
-      price: Number(Math.floor((Math.random() * 100).toFixed(2))),
-      quantity: Number(Math.floor((Math.random() * 100).toFixed(0))),
-      isPrime: false,
-      inCart: false,
-      cartQuantity: 0
-    };
-    data.push(obj);
-  }
-}
-
-let writeOneMillionTimes = (writer, data, encoding, callback) => {
+let writeOneMillionTimes = (writer, encoding, callback) => {
   let i = 10000000;
   write();
   function write() {
@@ -27,9 +9,18 @@ let writeOneMillionTimes = (writer, data, encoding, callback) => {
     do {
       i--;
       let index = 10000000 - 1 - i;
-      let newData = data[index].id + "," + data[index].name + "," + data[index].price + "," + data[index].quantity + "," + data[index].isPrime + "," + data[index].inCart + "," + data[index].cartQuantity + "\n";
+      let obj = {
+        id: index + 1,
+        name: `Amazon_Product${index + 1}`,
+        price: Number(Math.floor((Math.random() * 100).toFixed(2))),
+        quantity: Number(Math.floor((Math.random() * 100).toFixed(0))),
+        isPrime: false,
+        inCart: false,
+        cartQuantity: 0
+      };
+
+      let newData = obj.id + "," + obj.name + "," + obj.price + "," + obj.quantity + "," + obj.isPrime + "," + obj.inCart + "," + obj.cartQuantity + "\n";
       if (i === 0) {
-        // last time!
         writer.write(newData, encoding, callback);
       } else {
 
@@ -43,14 +34,11 @@ let writeOneMillionTimes = (writer, data, encoding, callback) => {
 }
 
 let startToWrite = () => {
-  let data = [];
-  feedToArr(data); // call function to feed in data to array. 
-  writeOneMillionTimes(wstream, data, "UTF-8", (err, data) => {
+  writeOneMillionTimes(wstream, "UTF-8", (err, data) => {
     if (err) {
       console.log(err);
       return;
     }
-
   })
 }
 
