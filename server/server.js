d4755b7ead5e5dd31628ec38b3710a6bef73f72f
EC2 Default User <ec2-user@ip-172-31-22-56.us-east-2.compute.internal>
2019-03-15 19:40:08 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,27 +1,40 @@
 // SERVER FILE
-const nr = require("newrelic");
+require("newrelic");
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
-// 
+const path = require('path'); 
 const app = express();
 const { getProduct } = require('../database/index.js');
 const { getItem } = require('../database/database.js');
-const { saveItem } = require('../database/database.js');
+const { saveItem } = require('../database/database.js'); 
+const morgan = require('morgan');
+const cache = {}; 
 
+app.use(morgan('tiny'));
 app.use(cors());
-
 app.use(express.static(`${__dirname}/../client/dist`));
+
+
+
+
 
 app.get('/api/product/:id', (req, res) => {
   const idToSearch = Number((req.params.id)) + 1;
-  getItem(idToSearch, (err, data) => {
-    if (err) {
-      res.status(400).send();
-      return;
-    }
-    res.status(200).send(data);
-  });
+   
+  if(cache[idToSearch]) {
+      return cache[idToSearch]; 
+  }
+
+  else {
+      getItem(idToSearch, (err, data) => {
+        if (err) {
+          res.status(400).send();
+          return;
+        }
+        cache[idToSearch] = data; 
+        res.status(200).send(data);
+      });
+  }
 });
 
 app.post('/post/saveItem', (req, res) => {
@@ -38,6 +51,5 @@ app.get('*', (req, res) => {
   res.sendFile(`/client/dist/index.html`, { 'root': `${__dirname}/../` });
 });
 
-const PORT = 3002;
-
-app.listen(PORT, console.log('listening at 3002'));
+const PORT = 80;
+app.listen(PORT, console.log(`listening at ${PORT} `));
